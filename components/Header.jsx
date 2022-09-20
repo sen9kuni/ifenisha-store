a8f4a163c579ced327c1f430aa0083532a558b7c
bimaap <let.bimaap@gmail.com>
2022-09-20 12:46:07 +0700

    fix: linking

--- components/Header.jsx
+++ components/Header.jsx
@@ -63,9 +63,11 @@ export default function Header(){
                         page.active&&
                         <div style={{top: page.top, left: page.left}} className={'absolute rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10'}>
                             <div className='py-1' role='none'>
-                                <a href='#' className='text-gray-700 block px-4 py-2 text-sm' role='menuitem' id='menu-item-0'>Account settings</a>
-                                <a href='#' className='text-gray-700 block px-4 py-2 text-sm' role='menuitem' id='menu-item-1'>Support</a>
-                                <a href='#' className='text-gray-700 block px-4 py-2 text-sm' role='menuitem' id='menu-item-2'>License</a>
+                                <a href='/main/About' className='text-gray-700 block px-4 py-2 text-sm' role='menuitem' id='menu-item-0'>About Us</a>
+                                <a href='/main/Contact' className='text-gray-700 block px-4 py-2 text-sm' role='menuitem' id='menu-item-1'>Contact Us</a>
+                                <a href='/main/Comingsoon' className='text-gray-700 block px-4 py-2 text-sm' role='menuitem' id='menu-item-2'>Coming Soon</a>
+                                <a href='#' className='text-gray-700 block px-4 py-2 text-sm' role='menuitem' id='menu-item-3'>404 Page</a>
+                                <a href='/main/Faq' className='text-gray-700 block px-4 py-2 text-sm' role='menuitem' id='menu-item-4'>FAQ Page</a>
                             </div>
                         </div>
                     }
